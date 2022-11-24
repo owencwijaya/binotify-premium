4c538f27862776e2f4c030c4ebfb1f8d50d0dfdf
hafidznrg <hafidznur.hn@gmail.com>
2022-11-25 00:46:18 +0700

    feat: add label in form login

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,32 +1,57 @@
 import { Flex, Heading } from "@chakra-ui/layout"
-import { Button, Image, Input } from "@chakra-ui/react"
+import { Alert, AlertDescription, AlertIcon, Button, FormControl, FormLabel, Image, Input } from "@chakra-ui/react"
 import { useState } from "react"
 import BinotifyLogo from "../assets/images/binotify.png"
 
 const LoginPage = () => {
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [error, setError] = useState(false)
+  const [errorMessage, setErrorMessage] = useState("")
+
+  const handleLogin = () => {
+    console.log("email:", email)
+    console.log("password:", password)
+    setEmail("")
+    setPassword("")
+  }
+  
     
   return (
       <Flex height="100vh" alignItems="center" justifyContent="center">
         <Flex direction="column" width="30vw" minWidth={300} background="gray.300" rounded={10} padding={4}>
           <Image src={BinotifyLogo} width="75%" m={5} alignSelf="center"/>
-          <Heading mb={5} color="green.700">Log In</Heading>
-          
-          <Input
-            placeholder="user@binotify.com"
-            type="email"
-            mb={3}
-            value={email}
-          />
+          <Heading mb={3} color="green.700">Log In</Heading>
 
-          <Input
-            placeholder="password"
-            type="password"
-            mb={3}
-            value={password}
-          />
-          <Button color="green.700">Log In</Button>
+          { error &&
+            <Alert status="error" my={4} rounded={4}>
+              <AlertIcon />
+              <AlertDescription fontSize="md">{errorMessage}</AlertDescription>
+            </Alert>
+          }
+          
+          <FormControl my={3}>
+            <FormLabel>Email</FormLabel>
+            <Input
+              placeholder="user@binotify.com"
+              type="email"
+              rounded={4}
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </FormControl>
+
+          <FormControl my={3}>
+            <FormLabel>Password</FormLabel>
+            <Input
+              placeholder="password"
+              type="password"
+              rounded={4}
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </FormControl>
+          <Button color="green.700" mt={3} onClick={handleLogin}>Log In</Button>
         </Flex>
       </Flex>
   )
