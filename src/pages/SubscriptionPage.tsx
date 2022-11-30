2c9846624ee44483cc10c4161efad0cdbcffc37e
hafidznrg <hafidznur.hn@gmail.com>
2022-11-30 22:51:52 +0700

    feat: add handle accept and decline

--- src/pages/SubscriptionPage.tsx
+++ src/pages/SubscriptionPage.tsx
@@ -1,6 +1,7 @@
-import { Center, Flex, Heading, IconButton, Table, TableContainer, Tbody, Th, Thead, Tr } from "@chakra-ui/react"
+import { Box, Button, Center, Flex, Heading, IconButton, Table, TableContainer, Tbody, Th, Thead, Tr, useToast } from "@chakra-ui/react"
 import axios from "axios"
 import { useEffect, useState } from "react"
+import { BiLogOut } from "react-icons/bi"
 import { BsCheck2Circle } from "react-icons/bs"
 import { MdClose } from "react-icons/md"
 import { Status } from "../interface/Status"
@@ -32,9 +33,10 @@ const requestList: SubscriptionRequest[] = [
 const SubscriptionPage = () => {
   const url = 'http://localhost:3000'
   const [requests, setRequests] = useState<SubscriptionRequest[]>([]);
+  const toast = useToast();
 
-  useEffect(() => { 
-    axios.get(`${url}/subscription`, {
+  useEffect(() => {
+    axios.get(`${url}/subs?limit=5&page=1`, {
       headers: {
         'Content-Type': 'application/json',
         'Authorization': `${sessionStorage.getItem("auth_token")}`
@@ -52,26 +54,91 @@ const SubscriptionPage = () => {
           status: inter.status
         })
       })
+      setRequests(subscriptionRequests)
     })
   })
 
-  const handleDelete = (subscriber_id: number | null, creator_id: number | null) => {
+  const handleDecline = (subscriber_id: number | null, creator_id: number | null) => {
     console.log("delete request from", subscriber_id, "to", creator_id)
+    axios.put(`${url}/subs/`, {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `${sessionStorage.getItem("auth_token")}`
+      },
+      data: {
+        'user_id': `${sessionStorage.getItem("user_id")}`,
+        'subscriber_id': `${subscriber_id}`,
+        'creator_id': `${creator_id}`,
+        'new_status': Status.REJECTED
+      }
+    }).then((response)=>{
+      console.log(response)
+      if (response.status === 200) {
+        console.log("success")
+        toast({
+          title: "Request declined",
+          description: "Success decline a request",
+          status: "success",
+          duration: 9000,
+          position: "top",
+          isClosable: true,
+        })
+      }
+    })
   }
 
-  const handleEdit = (subscriber_id: number | null, creator_id: number | null) => {
+  const handleAccept = (subscriber_id: number | null, creator_id: number | null) => {
     console.log("edit request from", subscriber_id, "to", creator_id)
     axios.put(`${url}/subs/`, {
       headers: {
         'Content-Type': 'application/json',
         'Authorization': `${sessionStorage.getItem("auth_token")}`
+      },
+      data: {
+        'user_id': `${sessionStorage.getItem("user_id")}`,
+        'subscriber_id': `${subscriber_id}`,
+        'creator_id': `${creator_id}`,
+        'new_status': Status.ACCEPTED
       }
     }).then((response)=>{
-
+      console.log(response)
+      if (response.status === 200) {
+        console.log("success")
+        toast({
+          title: "Request accepted",
+          description: "Success accepted a request",
+          status: "success",
+          duration: 9000,
+          position: "top"
+        })}
     })
+  }
+  
+  const handleLogout = () => {
+    axios.get(`${url}/logout`, {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `${sessionStorage.getItem("auth_token")}`
+      }
+    }).then((response)=>{
+      console.log("Response logout", response)
+      sessionStorage.clear()
+    })
+    window.location.href = "/login"
   }
 
   return (
+    <Box minHeight="100vh">
+    <Flex justifyContent="flex-start" mt={5} ml={5}>
+      <Button
+        leftIcon={<BiLogOut/>}
+        variant="solid"
+        colorScheme="red"
+        onClick={handleLogout}
+      >
+        Logout
+      </Button>
+    </Flex>
     <Center w="100vw">
     <Flex mt={20} direction="column" justifyContent="flex-start" alignItems="center" width="100%" height="100vh" pt={5}>
       <Heading color="green.700">Subscription Request</Heading>
@@ -106,7 +173,7 @@ const SubscriptionPage = () => {
                         bg="red.300"
                         mx={1}
                         size="sm"
-                        onClick={() => handleDelete(request.subscriber_id, request.creator_id)}
+                        onClick={() => handleDecline(request.subscriber_id, request.creator_id)}
                       />
                       <IconButton
                         aria-label="Edit Song"
@@ -114,7 +181,7 @@ const SubscriptionPage = () => {
                         bg="green.300"
                         mx={1}
                         size="sm"
-                        onClick={() => handleEdit(request.subscriber_id, request.creator_id)}
+                        onClick={() => handleAccept(request.subscriber_id, request.creator_id)}
                       />
                     </>
                   )}
@@ -147,6 +214,7 @@ const SubscriptionPage = () => {
     ) : <Heading size = 'md' mt = {10}>You don't have any requests yet!</Heading> }
     </Flex>
     </Center>
+    </Box>
   )
 }
 
